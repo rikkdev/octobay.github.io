e2ea4463a6b2e3336ec347966a2f2bb5ef6e3503
rikkce <l.riccardo@web.de>
2021-03-31 02:02:29 +0200

    added more videos and components

---
+++ components/Footer.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <!-- <div class="wave wave-footer"></div> -->
+    <footer class="text-center text-white">
+      <div class="container">
+        <div class="p-5 pt-0 text-white">
+          <div class="d-flex flex-column flex-md-row footer-links justify-content-between py-5">
+            <div class="d-flex flex-column align-items-center align-items-md-start">
+              <a
+                href="#"
+                class="btn text-white"
+              >Litepaper</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Whitepaper</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Documentation</a>
+            </div>
+            <div class="d-flex flex-column align-items-center align-items-md-start">
+              <a
+                href="#"
+                class="btn text-white"
+              >Projects</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Contributors</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Partners</a>
+            </div>
+            <div class="d-flex flex-column align-items-center align-items-md-start">
+              <a
+                href="#"
+                class="btn text-white"
+              >Terms of Service</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Privacy Policy</a>
+              <a
+                href="#"
+                class="btn text-white"
+              >Imprint</a>
+            </div>
+          </div>
+          <div class="social-icons">
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-reddit"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-instagram"></i>
+            </a>
+            <div class="d-block d-md-none"></div>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-facebook"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-google"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-linkedin"></i>
+            </a>
+            <div class="d-block d-md-none"></div>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-github"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-medium"></i>
+            </a>
+            <a
+              href="#"
+              class="btn text-white"
+            >
+              <i class="fab fa-telegram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>
---
+++ components/VideoClaimBounty.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="mb-5 pb-5 mb-md-0 pb-md-0 ps-md-5 ms-md-5 content-bg content-bg1 rellax"
+    v-rellax="{speed:0}"
+  >
+    <a id="smartbounties"></a>
+    <div class="w-100 d-flex justify-content-center justify-content-md-between">
+      <div
+        class="card p-md-5 p-4 mb-auto border-0 shadow-lg vw-50 rellax"
+        v-rellax="{speed:-1.3}"
+      >
+        <h4 class="accent-color">Innovative Marketplace to Outsource Tasks</h4>
+        <h1>Claim Bounties</h1>
+        <p class="m-0">
+          Octobay provided trustless bounties for open source projects hosted on GitHub.<br>
+          <br>
+          Each reward can be stored in a smart contract to act as a middleman between the customer and the developer.
+          Once an issue is closed, the contributor can claim his reward without additional consense from the maintainer.
+        </p>
+      </div>
+      <div
+        class="d-none d-md-block animation animation-right shadow-lg p-2 mb-auto bg-white vw-25 rellax"
+        v-rellax="{speed:-0.5}"
+      >
+        <!-- width="220"
+              height="425" -->
+        <video
+          style="width:25vw; height:28.295vw;"
+          autoplay="autoplay"
+          muted
+          loop
+        >
+          <source
+            src="video/claim-bounty.mp4"
+            type="video/mp4"
+          >
+        </video>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VideoClaimBounty",
+};
+</script>
+
+<style>
+</style>
